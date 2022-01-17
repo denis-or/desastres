eb4eb740a78eb41b7c0a806036b7f2d93aec6cbd
denis-or <catoper@gmail.com>
2022-01-17 18:22:54 -0300

    Corrigindo...

--- R/get_data_disaster.R
+++ R/get_data_disaster.R
@@ -12,6 +12,10 @@ scrape_disaster <- function(u){
 
   r <- httr::GET(u, httr::user_agent(user))
 
+  if(r$status_code != 200) {
+    stop("Erro de conexão com o site da Defesa Civil.")
+  }
+
   tabelas <- httr::content(r) |>
     xml2::xml_find_all("//table") |>
     rvest::html_table()
@@ -20,98 +24,131 @@ scrape_disaster <- function(u){
 
   tabela <- tabela[-c(1:2),]
 
-  colnames(tabela) <- c("ordem","data_entrada","municipio","decreto_nr","decreto_data",
-                        "decreto_vigencia","vencimento","tipo","desastre")
+  colnames(tabela) <- c("ordem","data_entrada","municipio",
+                        "decreto_nr","decreto_data", "decreto_vigencia",
+                        "vencimento","tipo","desastre")
 
-
-  coords <- silence_pls(
-    geobr::read_municipal_seat() |>
-      sf::st_centroid() |>
-      dplyr::mutate(id_municipio = as.character(code_muni))
-  )
-
+  coords <- readRDS('inst/br_pts.rds')
 
   tab_comp <-silence_pls(tabela |>
-                            munifacil::limpar_colunas(municipio, col_uf = "MG") |>
-                            munifacil::incluir_codigo_ibge())
+                           munifacil::limpar_colunas(municipio, col_uf = "MG") |>
+                           munifacil::incluir_codigo_ibge())
 
-  tab_comp_c <- tab_comp |>
-    dplyr::left_join(coords)|>
-    dplyr::mutate(lat = sf::st_coordinates(geom)[,2],
-                  lon = sf::st_coordinates(geom)[,1],
-                  # icon_df = ifelse(stringr::str_detect(desastre, "SECA"), "seca", "alag"),
-                  icon_df = factor(dplyr::case_when(
-                    stringr::str_detect(desastre, "SECA") ~ "Seca",
-                    stringr::str_detect(desastre, "ALAGAMEN") ~ "Alagamento",
-                    stringr::str_detect(desastre, "ENXURRA") ~ "Alagamento",
-                    stringr::str_detect(desastre, "CORRIDA") ~ "Deslizamento",
-                    stringr::str_detect(desastre, "DESLIZ") ~ "Deslizamento",
-                    stringr::str_detect(desastre, "ESTIAG") ~ "Seca",
-                    stringr::str_detect(desastre, "INCÊND") ~ "Incendio",
-                    stringr::str_detect(desastre, "INUN") ~ "Alagamento",
-                    stringr::str_detect(desastre, "FRIO") ~ "Frio",
-                    stringr::str_detect(desastre, "FRIA") ~ "Frio",
-                    stringr::str_detect(desastre, "QUEDA") ~ "Queda de rochas",
-                    stringr::str_detect(desastre, "COLAPSO") ~ "Queda de rochas",
-                    stringr::str_detect(desastre, "TEMPESTADE") ~ "Tempestade",
-                    stringr::str_detect(desastre, "DOENÇA") ~ "Doenca"
-                  )),
-                  image_df = paste0("images/",tolower(substr(icon_df, 1, 4)),".png")
-    ) |>
-    dplyr::select(1:9,26:29) |>
-    dplyr::filter(icon_df != "Tempestade",
-                  icon_df != "Seca")
-
+  tab_comp_c <- silence_pls(
+    tab_comp |>
+      dplyr::left_join(coords, by = "id_municipio") |>
+      dplyr::mutate(
+        icon_df = factor(dplyr::case_when(
+          stringr::str_detect(desastre, "SECA") ~ "Seca",
+          stringr::str_detect(desastre, "ALAGAMEN") ~ "Alagamento",
+          stringr::str_detect(desastre, "ENXURRA") ~ "Alagamento",
+          stringr::str_detect(desastre, "CORRIDA") ~ "Deslizamento",
+          stringr::str_detect(desastre, "DESLIZ") ~ "Deslizamento",
+          stringr::str_detect(desastre, "ESTIAG") ~ "Seca",
+          stringr::str_detect(desastre, "INCÊND") ~ "Incendio",
+          stringr::str_detect(desastre, "INUN") ~ "Alagamento",
+          stringr::str_detect(desastre, "FRIO") ~ "Frio",
+          stringr::str_detect(desastre, "FRIA") ~ "Frio",
+          stringr::str_detect(desastre, "QUEDA") ~ "Queda de rochas",
+          stringr::str_detect(desastre, "COLAPSO") ~ "Queda de rochas",
+          stringr::str_detect(desastre, "TEMPESTADE") ~ "Tempestade",
+          stringr::str_detect(desastre, "DOENÇA") ~ "Doenca"
+        )),
+        image_df = paste0("images/",tolower(substr(icon_df, 1, 4)),".png")
+      ) |>
+      subset(,
+             c(
+               ordem,
+               data_entrada,
+               municipio,
+               decreto_nr,
+               decreto_data,
+               decreto_vigencia,
+               vencimento,
+               desastre,
+               lat,
+               lon,
+               icon_df,
+               image_df
+             )
+      ) |>
+      dplyr::filter(icon_df != "Tempestade",
+                    icon_df != "Seca")
+  )
 
   tabela_temp <- tabelas[[1]]
 
   colnames(tabela_temp) <- c("ordem","municipio")
 
   tabela_temp_c <- tabela_temp |>
-    transform(desastre = "TEMPESTADE") |>
+    transform(desastre = "TEMPESTADE",
+              icon_df = "Tempestade") |>
     munifacil::limpar_colunas(municipio, col_uf = "MG") |>
     munifacil::incluir_codigo_ibge() |>
-    dplyr::left_join(coords)|>
-    dplyr::mutate(lat = sf::st_coordinates(geom)[,2],
-                  lon = sf::st_coordinates(geom)[,1],
-                  icon_df = "Tempestade",
-                  image_df = paste0("images/",tolower(substr(icon_df, 1, 4)),".png"),
-                  data_entrada = '06/01/2022',
-                  decreto_nr = NA_character_,
-                  decreto_data = NA_character_,
-                  decreto_vigencia = NA_character_,
-                  vencimento = NA_character_,
-                  tipo = NA_character_
+    dplyr::left_join(coords, by = "id_municipio") |>
+    transform(
+      image_df = paste0("images/", tolower(substr(icon_df, 1, 4)), ".png"),
+      data_entrada = '06/01/2022',
+      decreto_nr = NA_character_,
+      decreto_data = NA_character_,
+      decreto_vigencia = NA_character_,
+      vencimento = NA_character_
     ) |>
-    dplyr::select(ordem, data_entrada, municipio, decreto_nr,
-                  decreto_data, decreto_vigencia, vencimento,
-                  tipo, desastre, lat, lon, icon_df, image_df)
+    subset(,c(
+        ordem,
+        data_entrada,
+        municipio,
+        decreto_nr,
+        decreto_data,
+        decreto_vigencia,
+        vencimento,
+        desastre,
+        lat,
+        lon,
+        icon_df,
+        image_df
+      )
+    )
+
 
   tabela_temp2 <- tabelas[[2]]
 
   colnames(tabela_temp2) <- c("ordem","municipio")
 
   tabela_temp_c2 <- tabela_temp2 |>
-    transform(desastre = "SECA") |>
+    transform(desastre = "SECA",
+              icon_df = "Seca") |>
     munifacil::limpar_colunas(municipio, col_uf = "MG") |>
     munifacil::incluir_codigo_ibge() |>
-    dplyr::left_join(coords)|>
-    dplyr::mutate(lat = sf::st_coordinates(geom)[,2],
-                  lon = sf::st_coordinates(geom)[,1],
-                  icon_df = "Seca",
-                  image_df = paste0("images/",tolower(substr(icon_df, 1, 4)),".png"),
-                  data_entrada = '06/01/2022',
-                  decreto_nr = NA_character_,
-                  decreto_data = NA_character_,
-                  decreto_vigencia = NA_character_,
-                  vencimento = NA_character_,
-                  tipo = NA_character_
+    dplyr::left_join(coords, by = "id_municipio") |>
+    transform(
+      image_df = paste0("images/", tolower(substr(icon_df, 1, 4)), ".png"),
+      data_entrada = '06/01/2022',
+      decreto_nr = NA_character_,
+      decreto_data = NA_character_,
+      decreto_vigencia = NA_character_,
+      vencimento = NA_character_
     ) |>
-    dplyr::select(ordem, data_entrada, municipio, decreto_nr,
-                  decreto_data, decreto_vigencia, vencimento,
-                  tipo, desastre, lat, lon, icon_df, image_df)
+    subset(,
+      select = c(
+        ordem,
+        data_entrada,
+        municipio,
+        decreto_nr,
+        decreto_data,
+        decreto_vigencia,
+        vencimento,
+        desastre,
+        lat,
+        lon,
+        icon_df,
+        image_df
+      )
+    )
+
 
   tab_comp_c <- rbind(tab_comp_c, tabela_temp_c, tabela_temp_c2)
+
 
   tab_comp_c$dt_atu <- httr::content(r) |>
     xml2::xml_find_all(xpath = ".//p") |>
@@ -133,4 +170,5 @@ get_data_disaster <- function() {
 
 }
 
+df <- get_data_disaster()
 
